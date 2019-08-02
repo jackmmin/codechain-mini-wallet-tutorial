e820020c18276ef015d9e7afa61a598bc45781f4
jackmmin <dpeltms14@naver.com>
2019-08-02 17:19:55 +0900

    mod_watch.ts_내가보낸, 내가받은 트랜잭션 출력

--- src/commands/watch.ts
+++ src/commands/watch.ts
@@ -2,12 +2,18 @@ import { SDK } from "codechain-sdk";    // sdk 모듈 불러오기
 
 import { Tracer } from "../tracer";
 import { Pay, PlatformAddress, U64 } from "codechain-sdk/lib/core/classes";
+import account from "./account";
 
 export default async function watch(
     sdk: SDK,
     tracer: Tracer,
     args: string[]
 ) {
+    const address = tracer.state.address;
+    if( address === undefined ) {
+        throw new Error( "init을 하세요" );
+    }
+
     let from;
     if( args.length > 0 ) {
         from = parseInt( args[ 0 ], 10 );
@@ -15,16 +21,11 @@ export default async function watch(
         from = await sdk.rpc.chain.getBestBlockNumber();
     }
 
-    // const blockNumber = parseInt( args[ 0 ], 10 );
-
     for( let blockNumber = from; ; blockNumber++ ) {
         const block = await eventuallyGetBlock( sdk, blockNumber );
-    
-        console.log( "Block number:", block.number );
-        console.log( "Block hash:", block.hash.toString() );
 
         for( const tx of block.transactions ) { // pay 트랜잭션이 발생할 때 출력한다.
-            console.log( "Tx", tx.unsigned.type(), tx.hash().toString() );
+            // console.log( "Tx", tx.unsigned.type(), tx.hash().toString() );
             if( tx.unsigned.type() === "pay" ) {
                 const { receiver, quantity }: {
                     receiver: PlatformAddress,
@@ -33,9 +34,24 @@ export default async function watch(
                 const sender = tx.getSignerAddress( {
                     networkId: "wc",
                 });
-                console.log( "  Sender:", sender.toString() );
-                console.log( "  Receiver:", receiver.toString() );
-                console.log( "  Quantity:", quantity.toString() );
+
+                if( sender.toString() == address.platformAddress.toString() ) {
+                    // 내가 보낸 pay 트랜잭션
+                    console.log( {
+                        type: "send",
+                        receiver: receiver.toString(),
+                        quantity: quantity.toString(),
+                        fee: tx.unsigned.fee()!.toLocaleString(),
+                    });
+                }
+                if( receiver.toString() == address.platformAddress.toString() ) {
+                    // 내가 받은 pay 트랜잭션
+                    console.log( {
+                        type: "receive",
+                        sender: sender.toString(),
+                        quantity: quantity.toString(),
+                    });
+                }
             }
         }
     }
